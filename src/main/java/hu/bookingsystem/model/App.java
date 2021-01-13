c1e23610cb630a06e6ca47d9058b3feb8484360f
Jozsef Nagy <jozsefnagy141@gmail.com>
2021-01-13 20:10:41 +0100

    Project initialization.

---
+++ src/main/java/hu/bookingsystem/model/App.java
@@ -0,0 +1,13 @@
+package hu.bookingsystem.model;
+
+/**
+ * Init project
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Project!" );
+    }
+}
