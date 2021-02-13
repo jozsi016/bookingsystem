4d8edc4ec83bf213a843dda81029d18fd5ba0feb
Jozsef Nagy <jozsefnagy141@gmail.com>
2021-02-13 11:26:30 +0100

    Clean up on ReservationRepositoryTes

--- src/test/java/hu/bookingsystem/repository/ReservationRepositoryTest.java
+++ src/test/java/hu/bookingsystem/repository/ReservationRepositoryTest.java
@@ -4,10 +4,15 @@ import hu.bookingsystem.model.Reservation;
 import hu.bookingsystem.model.Room;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 public class ReservationRepositoryTest {
 
@@ -21,14 +26,16 @@ public class ReservationRepositoryTest {
     @Test
     public void shouldAddReservation() {
         //Given
-        long expected = 1;
         Room room = new Room(1L, 5000);
         double price = room.getUnitPrice() * 5;
-        Reservation reservation = new Reservation(1L, 1L, room.getId(), LocalDate.now().minusDays(5), LocalDate.now(), price);
-        repo.addReservation(reservation);
+        Reservation expected = new Reservation(1L, 1L, room.getId(), LocalDate.now().minusDays(5), LocalDate.now(), price);
+        ReservationRepository repoSpy = spy(repo);
         //When
-        long actual = repo.getReservations().get(1L).getId();
+        repoSpy.addReservation(expected);
+        Reservation actual = repoSpy.getReservations().get(1L);
         //Then
-        assertEquals(expected, actual);
+      //  assertEquals(expected, actual);
+        assertThat(actual, is(expected));
+        verify(repoSpy).addReservation(expected);
     }
 }
