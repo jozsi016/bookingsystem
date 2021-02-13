1d001a7b2947da6dad6c894848498ce2f9e0c99f
Jozsef Nagy <jozsefnagy141@gmail.com>
2021-02-13 11:27:28 +0100

    Start implement rest api test for rooms

--- src/test/java/hu/bookingsystem/controller/RoomControllerMvcTest.java
+++ src/test/java/hu/bookingsystem/controller/RoomControllerMvcTest.java
@@ -1,2 +1,35 @@
-package hu.bookingsystem.controller;public class RoomControllerMvcTest {
+package hu.bookingsystem.controller;
+
+import hu.bookingsystem.model.Room;
+import hu.bookingsystem.service.RoomService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RoomController.class)
+public class RoomControllerMvcTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private RoomService roomServiceMock;
+
+
+    @Test
+    public void shouldReturnRoom() throws Exception {
+        Room room = new Room(3, 5000);
+        String expected = "{\"id\":3,\"unitPrice\":5000.0}";
+        Mockito.when(roomServiceMock.getRoomById(any())).thenReturn(room);
+        this.mockMvc.perform(get("/room/3")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(expected)));
+    }
 }
--- src/test/java/hu/bookingsystem/controller/RoomControllerTest.java
+++ src/test/java/hu/bookingsystem/controller/RoomControllerTest.java
@@ -1,2 +1,31 @@
-package hu.bookingsystem.controller;public class RoomControllerTest {
+package hu.bookingsystem.controller;
+
+import hu.bookingsystem.model.Room;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RoomControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldReturnRoom() throws Exception {
+        Room expected = new Room(3, 5000);
+        Room actual =   this.restTemplate.getForObject("http://localhost:" + port + "/room/3", Room.class);
+        assertThat(actual, is(expected));
+    }
+
+
 }
+
